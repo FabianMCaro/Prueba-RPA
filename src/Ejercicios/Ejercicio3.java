31d3db2bbbb40bca4ba6a18de2d2549080ad982a
Fabian Caro <fabiancaro101@gmail.com>
2024-02-21 17:52:04 -0500

    Ejercicio 3

---
+++ src/Ejercicios/Ejercicio3.java
@@ -0,0 +1,30 @@
+package Ejercicios;
+
+import java.util.*;
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        System.out.println("Ejercicio #2\n");
+
+        List<Integer> list1 = List.of(12, 25, 1, 1, 7, 25);
+        System.out.println("Entrada: list="+list1);
+        System.out.println("Salida: " + grupoSimilares(list1)+"\n");
+
+        List<Integer> list2 = List.of(6, 7, 8, 9,9);
+        System.out.println("Entrada: list="+list2);
+        System.out.println("Salida: " + grupoSimilares(list2));
+    }
+
+    public static List<List<Integer>> grupoSimilares(List<Integer> list) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int numero : list) {
+            if (map.containsKey(numero)) {
+                map.get(numero).add(numero);
+            } else {
+                List<Integer> newList = new ArrayList<>();
+                newList.add(numero);
+                map.put(numero, newList);
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+}
